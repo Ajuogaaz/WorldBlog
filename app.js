96bf3e221cbb464459077f942dd48f16fede546a
Ajuogaaz <brianlinus1753@gmail.com>
2020-09-01 16:45:22 -0400

    Added remote database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const port = 3000;
 const posts = [ ];
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,13 +15,34 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+const url = 'mongodb://127.0.0.1:27017/world-blog'
+//mongoose.connect("mongodb+srv://admin-linus:Test123@cluster0.auhfn.azure.mongodb.net/todolistDB", { useNewUrlParser: true,  useUnifiedTopology: true });
+mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+const BlogSchema = mongoose.Schema({
+  name : String,
+  body : String
+});
+
+const Blog = mongoose.model("Blog", BlogSchema);
+
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
 app.get("/" , (req, res)=>{
 
-  res.render("home", {firstBlog : homeStartingContent, Posts: posts});
+  Blog.find({}, (err, docs) => {
+    if(!err){
+      res.render("home", {firstBlog : homeStartingContent, Posts: docs});
+    }else{
+      console.log(err);
+    }
+  });
+
+
 
 });
 
@@ -37,24 +59,32 @@ app.get("/compose", (req, res) => {
 });
 app.post("/compose", (req, res) =>{
 
-  const blogInput = {
-    title : req.body.PostTitle,
+  const blogInput = new Blog({
+    name : req.body.PostTitle,
     body : req.body.PostBody
-  }
-  posts.push(blogInput);
-  res.redirect("/");
+  });
+
+  blogInput.save( (err)=>{
+    if(!err){
+      res.redirect("/");
+    }
+  });
+
 
 })
 
 app.get("/:topic", (req, res) => {
-  posts.forEach( (post)=> {
-    if(_.lowerCase(post.title) === _.lowerCase(req.params.topic)){
-        res.render("post", {title : post.title, body : post.body })
+
+  const route = _.capitalize(req.params.topic);
+
+  Blog.findOne({name : route}, (err, doc) => {
+    if(!err && doc !== null){
+      res.render("post", {name : doc.name, body : doc.body })
     }
   });
 
 });
 
 app.listen( process.env.PORT || port, function() {
-  //console.log(`listening at http://localhost:${port}`);
+  console.log(`listening at http://localhost:${port}`);
 });
